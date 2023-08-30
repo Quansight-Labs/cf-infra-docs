e9231f46d91979c0338bcf68aacf5000898fd7af
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-08-30 11:23:13 +0530

    feat: add about section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import Header from '@site/src/components/Header';
+import Header from "@site/src/components/Header";
+import About from "@site/src/components/About";
 
 //import styles from "./modules/index.module.css";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={siteConfig.title}
-      description="Documentation for conda-forge infrastructure">
-      <main>
-        <Header />
-      </main>
-    </Layout>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout
+            title={siteConfig.title}
+            description="Documentation for conda-forge infrastructure"
+        >
+            <main>
+                <Header />
+                <About />
+            </main>
+        </Layout>
+    );
 }
