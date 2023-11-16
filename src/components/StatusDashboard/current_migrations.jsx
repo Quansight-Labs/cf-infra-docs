347610ca6940ff8fd58e8b74942b564c9e8e3705
Afshin T. Darian <git@darian.email>
2023-11-16 10:56:11 +0000

    Reshape response data into a more easily consumable format

--- src/components/StatusDashboard/current_migrations.jsx
+++ src/components/StatusDashboard/current_migrations.jsx
@@ -2,35 +2,31 @@ import React, { useEffect, useState } from "react";
 import { urls } from "../../constants";
 
 function FullView(state) {
-  console.log('state', state);
-  return (
-      <div className="card__body">
-        work in progress
-      </div>
-  );
+  console.log("state", state);
+  return <div className="card__body">work in progress</div>;
 }
 
-function SummaryView({longterm, regular, closed}) {
+function SummaryView({ longterm, regular, closed }) {
   return (
-      <div className="card__body">
-        <div className="row">
-          <div className="col col--4">
-            <div className="migration">
-              Long-running migrations ({longterm.length || ""})
-            </div>
+    <div className="card__body">
+      <div className="row">
+        <div className="col col--4">
+          <div className="migration">
+            Long-running migrations ({longterm.length || ""})
           </div>
-          <div className="col col--4">
-            <div className="migration">
-              Regular migrations ({regular.length || ""})
-            </div>
+        </div>
+        <div className="col col--4">
+          <div className="migration">
+            Regular migrations ({regular.length || ""})
           </div>
-          <div className="col col--4">
-            <div className="migration">
-              Closed migrations ({closed.length || ""})
-            </div>
+        </div>
+        <div className="col col--4">
+          <div className="migration">
+            Closed migrations ({closed.length || ""})
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
@@ -40,9 +36,9 @@ export default function CurrentMigrations() {
     loaded: false,
     longterm: [],
     regular: [],
-    summary: true
+    summary: true,
   });
-  const toggle = event => {
+  const toggle = (event) => {
     event.preventDefault();
     setState({ ...state, summary: !state.summary });
   };
@@ -56,7 +52,9 @@ export default function CurrentMigrations() {
       for (const key in urls.migrations.status) {
         try {
           const response = await fetch(urls.migrations.status[key]);
-          state[key] = Object.keys(await response.json());
+          state[key] = Object.entries(await response.json()).map(
+            ([name, description]) => ({ name, description })
+          );
         } catch (error) {
           console.warn("error loading current migrations", error);
         }
@@ -70,10 +68,12 @@ export default function CurrentMigrations() {
       <div className="card__header">
         <h3>
           Current Migrations
-          <a href="" onClick={toggle}>View all migrations</a>
+          <a href="" onClick={toggle}>
+            View all migrations
+          </a>
         </h3>
       </div>
-      {state.summary ? (<SummaryView {...state} />) : (<FullView {...state} />)}
+      {state.summary ? <SummaryView {...state} /> : <FullView {...state} />}
     </div>
   );
 }
