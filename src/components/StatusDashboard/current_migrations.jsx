ca3b0133eeb365fb89f83a9efde37ba0b8d40b8a
Afshin T. Darian <git@darian.email>
2023-11-19 23:18:10 +0000

    Rational collapsed state

--- src/components/StatusDashboard/current_migrations.jsx
+++ src/components/StatusDashboard/current_migrations.jsx
@@ -1,34 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { urls } from "../../constants";
 
-function Full({ longterm, regular, closed, style }) {
+function Full({ closed, collapsed, longterm, regular, select, style }) {
   return (
     <div className="card__body" style={style}>
       <table>
-        <TableContent name="Long-running migrations" rows={longterm} />
-        <TableContent name="Regular migrations" rows={regular} />
-        <TableContent name="Closed migrations" rows={closed} />
+        <TableContent
+          collapsed={collapsed.longterm}
+          name="Long-running migrations"
+          rows={longterm}
+          select={() => select("longterm")}
+        />
+        <TableContent
+          collapsed={collapsed.regular}
+          name="Regular migrations"
+          rows={regular}
+          select={() => select("regular")}
+        />
+        <TableContent
+          collapsed={collapsed.closed}
+          name="Closed migrations"
+          rows={closed}
+          select={() => select("closed")}
+        />
       </table>
     </div>
   );
 }
 
-function Summary({ longterm, regular, closed, style }) {
+function Summary({ closed, longterm, regular, select, style }) {
   return (
     <div className="card__body" style={style}>
       <div className="row">
         <div className="col col--4">
-          <div className="migration">
+          <div className="migration" onClick={() => select("longterm")}>
             Long-running migrations ({longterm.length || ""})
           </div>
         </div>
         <div className="col col--4">
-          <div className="migration">
+          <div className="migration" onClick={() => select("regular")}>
             Regular migrations ({regular.length || ""})
           </div>
         </div>
         <div className="col col--4">
-          <div className="migration">
+          <div className="migration" onClick={() => select("closed")}>
             Closed migrations ({closed.length || ""})
           </div>
         </div>
@@ -37,25 +52,24 @@ function Summary({ longterm, regular, closed, style }) {
   );
 }
 
-function TableContent({ collapsed = false, name, rows }) {
-  const [{ toggled }, setState] = useState({ toggled: !collapsed });
+function TableContent({ collapsed, name, rows, select }) {
   return (
     <>
       <thead>
-        <tr onClick={() => setState({ toggled: !toggled })}>
+        <tr onClick={select}>
           <th colSpan="4">
             {name}
-            {toggled ? "" : "…"}
+            {collapsed ? "…" : ""}
           </th>
         </tr>
-        <tr style={toggled ? undefined : { display: "none" }}>
+        <tr style={collapsed ? { display: "none" } : undefined}>
           <th>Name</th>
           <th>Status</th>
           <th>Awaiting parents</th>
           <th>Awaiting PR</th>
         </tr>
       </thead>
-      <tbody style={toggled ? undefined : { display: "none" }}>
+      <tbody style={collapsed ? { display: "none" } : undefined}>
         {rows.map((row) => (
           <tr key={row.name}>
             <td>{row.description}</td>
@@ -72,6 +86,11 @@ function TableContent({ collapsed = false, name, rows }) {
 export default function CurrentMigrations() {
   const [state, setState] = useState({
     closed: [],
+    collapsed: {
+      closed: true,
+      longterm: true,
+      regular: true,
+    },
     loaded: false,
     longterm: [],
     regular: [],
@@ -79,9 +98,27 @@ export default function CurrentMigrations() {
   });
   const toggle = (event) => {
     event.preventDefault();
-    setState({ ...state, summary: !state.summary });
+    setState((prev) => ({ ...prev, summary: !state.summary }));
   };
-
+  const select = (selection) =>
+    setState((prev) => ({
+      ...prev,
+      collapsed: {
+        closed:
+          selection === "closed"
+            ? !prev.collapsed.closed
+            : prev.collapsed.closed,
+        longterm:
+          selection === "longterm"
+            ? !prev.collapsed.longterm
+            : prev.collapsed.longterm,
+        regular:
+          selection === "regular"
+            ? !prev.collapsed.regular
+            : prev.collapsed.regular,
+      },
+      summary: false,
+    }));
   useEffect(() => {
     if (state.loaded) {
       return;
@@ -102,15 +139,15 @@ export default function CurrentMigrations() {
           let index = 0;
           for (const { name } of fetched[status]) {
             promises.push(
-              (async (status, name, index) => {
+              (async () => {
                 try {
                   const url = urls.migrations.details.replace("<NAME>", name);
                   const response = await fetch(url);
-                  fetched[status][index].details = await response.json();
+                  fetched[status][index++].details = await response.json();
                 } catch (error) {
                   console.warn(`error loading migration: ${name}`, error);
                 }
-              })(status, name, index++)
+              })()
             );
           }
         } catch (error) {
@@ -118,7 +155,7 @@ export default function CurrentMigrations() {
         }
       }
       await Promise.all(promises);
-      setState({ ...state, ...fetched, loaded: true });
+      setState((prev) => ({ ...prev, ...fetched, loaded: true }));
     })();
   });
 
@@ -134,10 +171,12 @@ export default function CurrentMigrations() {
       </div>
       <Summary
         {...state}
+        select={select}
         style={state.summary ? undefined : { display: "none" }}
       />
       <Full
         {...state}
+        select={select}
         style={state.summary ? { display: "none" } : undefined}
       />
     </div>
