3648c4c6225a7f5fb7220eb26cded10eb7175d53
Afshin T. Darian <git@darian.link>
2024-02-15 19:20:48 +0000

    Start hitting the github API

--- src/components/StatusDashboard/incidents.jsx
+++ src/components/StatusDashboard/incidents.jsx
@@ -1,8 +1,32 @@
 import { React, useEffect } from "react";
+import { Octokit } from "octokit";
 import styles from "./styles.module.css";
 
+const BAD_LABELS = new Map([
+  ["investigating", null],
+  ["degraded performance", null],
+  ["major outage", null],
+  ["maintenance", null]
+]);
+
 export default function Incidents({ onLoad }) {
-  useEffect(() => void onLoad(), []);
+  useEffect(() => {
+    (async () => {
+      const octokit = new Octokit({});
+      try {
+        const issues = await octokit.rest.issues.listForRepo({
+          owner: "conda-forge",
+          repo: "status",
+          state: "all",
+          per_page: 100
+        });
+        console.log("issues", issues.data.map(({ labels }) => (labels)));
+      } catch (error) {
+        console.warn(`error loading github issues`, error);
+      }
+      onLoad();
+    })();
+  }, []);
   return (
     <>
       <div id="incidents" className={styles.toc_anchor}></div>
